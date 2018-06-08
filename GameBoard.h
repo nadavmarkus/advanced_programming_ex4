eacc92fcfbfebc4b272b4ee82b75ce3fb2148f8e
Nadav <markus.nadav@gmail.com>
2018-06-08 21:49:09 +0300

    Completed implementation.

--- GameBoard.h
+++ GameBoard.h
@@ -11,44 +11,74 @@ template <int ROWS, int COLS, class GAME_PIECE, int PLAYER_COUNT = 2>
 class GameBoard
 {
 private:
-    
     struct BoardPiece {
     public:
         int player;
         std::unique_ptr<GAME_PIECE> piece;
-    BoardPiece() : player(-1), piece(nullptr) {}
-    BoardPiece(const BoardPiece &other): player(other.player),
-                                         piece(nullptr == other.piece ? nullptr : std::make_unique<GAME_PIECE>(*other.piece)) {}
-    BoardPiece(int player, const GAME_PIECE &piece): player(player), piece(std::make_unique<GAME_PIECE>(piece)) {}
-    const BoardPiece& operator=(const BoardPiece& other)
-    {
-        if (this != &other) {
-            player = other.player;
-            if (nullptr != other.piece) {
-                piece = std::make_unique<GAME_PIECE>(*other.piece);
-            } else {
-                piece = nullptr;
-            }
-        }
         
-        return *this;
-    }
+        BoardPiece() : player(-1), piece(nullptr) {}
+        
+        BoardPiece(const BoardPiece &other): player(other.player),
+                                             piece(nullptr == other.piece
+                                                   ? nullptr
+                                                   : std::make_unique<GAME_PIECE>(*other.piece)) {}
+                                                   
+        BoardPiece(int player, const GAME_PIECE &piece): player(player), piece(std::make_unique<GAME_PIECE>(piece)) {}
+        
+        const BoardPiece& operator=(const BoardPiece& other)
+        {
+            if (this != &other) {
+                player = other.player;
+                if (nullptr != other.piece) {
+                    piece = std::make_unique<GAME_PIECE>(*other.piece);
+                } else {
+                    piece = nullptr;
+                }
+            }
+            
+            return *this;
+        }
     };
     
     BoardPiece board[ROWS][COLS];
     
 public:
     typedef std::tuple<int, int, GAME_PIECE, int> iterated_value;
+    
     class iterator {
     friend class GameBoard;
     private:
         int cur_row;
         int cur_col;
         const GameBoard &board;
+        int player_to_match;
+        std::unique_ptr<GAME_PIECE> piece_to_match;
         
         void getToNextPiece()
         {
-            while (cur_row < ROWS && nullptr == board.board[cur_row][cur_col].piece) {
+            while (cur_row < ROWS) {
+                const BoardPiece &cur_piece = board.board[cur_row][cur_col];
+                
+                /* Is this a valid piece? */
+                if (nullptr != cur_piece.piece) {
+                    bool should_break = true;
+                    
+                    /* Were we required to match on a specific player, and if so, do we match? */
+                    if (-1 != player_to_match && player_to_match != cur_piece.player) {
+                        should_break = false;
+                    }
+                    
+                    /* Were we required to match on a specific piece type, and if so, do we match? */
+                    if (nullptr != piece_to_match && *piece_to_match != *cur_piece.piece) {
+                        should_break = false;
+                    }
+                    
+                    if (should_break) {
+                        /* This means we matched on all criteria. */
+                        break;
+                    }
+                }
+            
                 cur_col = (cur_col + 1) % COLS;
                 
                 if (0 == cur_col) {
@@ -57,13 +87,29 @@ public:
             }
         }
         
-        iterator(int cur_row, int cur_col, const GameBoard &board): cur_row(cur_row),
-                                                                    cur_col(cur_col),
-                                                                    board(board)
+        iterator(int cur_row,
+                 int cur_col,
+                 const GameBoard &board,
+                 int player_to_match,
+                 std::unique_ptr<GAME_PIECE> piece_to_match): cur_row(cur_row),
+                                                              cur_col(cur_col),
+                                                              board(board),
+                                                              player_to_match(player_to_match),
+                                                              piece_to_match(nullptr == piece_to_match
+                                                                             ? nullptr
+                                                                             : std::make_unique<GAME_PIECE>(*piece_to_match))
         {
             getToNextPiece();
         }
     public:
+        iterator(const iterator &other): cur_row(other.cur_row),
+                                         cur_col(other.cur_col),
+                                         board(other.board),
+                                         player_to_match(other.player_to_match),
+                                         piece_to_match(nullptr == other.piece_to_match
+                                                        ? nullptr
+                                                        : std::make_unique<GAME_PIECE>(*other.piece_to_match))
+                                         {}
         /* Prefix increment */
         iterator operator++() {
             cur_col = (cur_col + 1) % COLS;
@@ -103,11 +149,11 @@ public:
     };
     
     iterator begin() {
-        return iterator(0, 0, *this);
+        return iterator(0, 0, *this, -1, nullptr);
     }
 
     iterator end() {
-        return iterator(ROWS, 0, *this);
+        return iterator(ROWS, 0, *this, -1, nullptr);
     }
 
     PieceInfo<GAME_PIECE> getPiece(int row, int col)
@@ -130,6 +176,41 @@ public:
         }
         
         return std::make_unique<const std::pair<int, GAME_PIECE>>(previous.player, *previous.piece);
+    }
+    
+    class iterator_wrapper {
+        friend class GameBoard;
+        private:
+            iterator start;
+            iterator fin;
+            iterator_wrapper(iterator &start, iterator &fin) : start(start), fin(fin) {}
+            
+        public:
+            iterator begin() { return start; }
+            iterator end() { return fin; }
+            
+            iterator_wrapper(const iterator_wrapper &other): start(other.start), fin(other.fin) {}
+    };
+    
+    iterator_wrapper allPiecesOfPlayer(int player)
+    {
+        iterator start(0, 0, *this, player, nullptr);
+        iterator fin(end());
+        return iterator_wrapper(start, fin);
+    }
+    
+    iterator_wrapper allOccureneceOfPiece(const GAME_PIECE& piece)
+    {
+        iterator start(0, 0, *this, -1, std::make_unique<GAME_PIECE>(piece));
+        iterator fin(end());
+        return iterator_wrapper(start, fin);
+    }
+    
+    iterator_wrapper allOccureneceOfPieceForPlayer(const GAME_PIECE& piece, int player)
+    {
+        iterator start(0, 0, *this, player, std::make_unique<GAME_PIECE>(piece));
+        iterator fin(end());
+        return iterator_wrapper(start, fin);
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main()
     GameBoard<3,3, char> gameboard;
     gameboard.setPiece(0,0, 'A', 1);
     
-    gameboard.setPiece(2,0, 'B', 2);
+    gameboard.setPiece(2,0, 'A', 2);
     
     gameboard.setPiece(0,1, 'C', 2);
     
@@ -18,4 +18,18 @@ int main()
         std::cout << std::get<0>(element) << "," << std::get<1>(element) << ": " << std::get<2>(element) << "-" << std::get<3>(element) << std::endl;
     }
     std::cout << std::endl;
+    
+    for (auto element: gameboard.allPiecesOfPlayer(2)) {
+        std::cout << std::get<0>(element) << "," << std::get<1>(element) << ": " << std::get<2>(element) << "-" << std::get<3>(element) << std::endl;
+    }
+    
+    std::cout << "Searching by piece" << std::endl;
+    for (auto element: gameboard.allOccureneceOfPiece('A')) {
+        std::cout << std::get<0>(element) << "," << std::get<1>(element) << ": " << std::get<2>(element) << "-" << std::get<3>(element) << std::endl;
+    }
+    
+    std::cout << "Searching by piece and player" << std::endl;
+    for (auto element: gameboard.allOccureneceOfPieceForPlayer('C', 2)) {
+        std::cout << std::get<0>(element) << "," << std::get<1>(element) << ": " << std::get<2>(element) << "-" << std::get<3>(element) << std::endl;
+    }
 }
